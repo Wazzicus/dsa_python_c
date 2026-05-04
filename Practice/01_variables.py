0d54724b79d3f9ef413cea29d97d5edd883a8e78
Emmanuel <princeabelle5@gmail.com>
2026-05-04 10:32:59 +0300

    add initial project files and configurations

---
+++ Practice/01_variables.py
@@ -0,0 +1,26 @@
+print("Hello World")
+
+name = "John"
+name2 = "John"
+num = 25
+balance = 159.258
+flag = True
+
+print(id(name))
+print(id(name2))
+
+print(type(name))
+print(type(num))
+print(type(balance))
+print(type(flag))
+
+print("Using range")
+for i in range(5):
+    print(i)
+
+
+
+print("Using for loop")
+items = [1,2,3,4,5,6,7,8]
+for item in items:
+    print(item)
